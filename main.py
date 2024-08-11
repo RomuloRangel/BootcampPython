5f0c070bd110fdcc170b45539af47feafe0b8ca4
Romulo Rangel <romulordocumentos@gmail.com>
2024-08-11 19:16:33 -0300

    Commit ajustanto try e except

--- main.py
+++ main.py
@@ -3,37 +3,47 @@ try:
     nome = input('Digite seu nome: ')
 
     if len(nome) == 0:
-        print('O nome não pode ser vazio')
-    elif isinstance(nome, str):
-        print('Nome valido')
+        raise ValueError('O nome não pode ser vazio')
+        exit()
+
+    # Verifica se há digito nos Input de nome
+    elif any(char.isdigit() for char in nome):
+        raise ValueError("O nome não deve conter números.")
+        exit()
     else:
-        print('Nome invalido ! ')
+        print("Nome válido:", nome)
 except ValueError as e:
-    print('e')
+    print(e)
+    exit()
     
 
 
 
 # 2) Solicita ao usuário que digite o valor do seu salário
 
-# Converte a entrada para um número de ponto flutuante
+
 try:
     
     salario = float(input('Digite seu salario: '))
+    if salario <= 0:
+        print('O valor não pode ser negativo ! ') 
+except ValueError:
+    print("Digite um valor válido")
+    exit()
 
-    if salario < 0:
-        print('O valor não pode ser negativo ! ')
-    else:
-        print('Salario valido')
-        
-except ValueError as e:
-    print(f'Valor do error Correspode a {e}')
 
 # 3) Solicita ao usuário que digite o valor do bônus recebido
-# Converte a entrada para um número de ponto flutuante
 
 
-bonus = float(input('Digite valor do bonus: '))
+try:
+    bonus = float(input('Digite valor do bonus: '))
+    if bonus < 0:
+        print("O valor do bonus não pode ser negativo")
+        exit()
+except ValueError:
+    print("Digite um valor válido")
+    exit()
+
 
 # 4) Calcule o valor do bônus final 
 
@@ -44,6 +54,5 @@ valor_bonus_final = (1000 + salario) * bonus
 print(f'O valor do bonus final será de : {valor_bonus_final}')
 
 # 6) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
-print(f'{nome} seu valor de bonus final será de {valor_bonus_final}')
+print(f'{nome}, seu salario é de {salario} e valor de bonus final será de {valor_bonus_final}')
 
-# Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
