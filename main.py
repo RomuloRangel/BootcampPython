c4c2e281d2ec3c4bd675275356b9e1f181f8a423
Romulo Rangel <romulordocumentos@gmail.com>
2024-08-17 11:34:01 -0300

    Commit implementando estruturas de repetições testando possiveis falhas que possam quebrar o codigo

--- main.py
+++ main.py
@@ -1,58 +1,62 @@
-# 1) Solicita ao usuário que digite seu nome
-try:
-    nome = input('Digite seu nome: ')
+nome_valido = False
+salario_valido = False
+bonus_valido = False
 
-    if len(nome) == 0:
-        raise ValueError('O nome não pode ser vazio')
-        exit()
+while nome_valido is not True:
 
-    # Verifica se há digito nos Input de nome
-    elif any(char.isdigit() for char in nome):
-        raise ValueError("O nome não deve conter números.")
-        exit()
-    else:
-        print("Nome válido:", nome)
-except ValueError as e:
-    print(e)
-    exit()
+    try:
+        nome = input('Digite seu nome: ').strip()
+
+        if len(nome) == 0:
+            raise ValueError('O nome não pode ser vazio')
+           
+
+        # Verifica se há digito nos Input de nome
+        elif any(char.isdigit() for char in nome):
+            raise ValueError("O nome não deve conter números.")
+           
+        else:
+            nome_valido = True
+            print("Nome válido:", nome)
+    except ValueError as e:
+        print(e)
     
-
-
+  
+    
 
 # 2) Solicita ao usuário que digite o valor do seu salário
 
+while salario_valido is not True:
+    try:
+        salario = input('Digite seu salario ').strip()
+        salario_novo = float(salario)
 
-try:
+        if salario_novo <= 0:
+            print('Não é aceito valor nulo ou menor que zero' )
+        
+        else:
+            salario_valido = True
+            print('Salario valido')
     
-    salario = float(input('Digite seu salario: '))
-    if salario <= 0:
-        print('O valor não pode ser negativo ! ') 
-except ValueError:
-    print("Digite um valor válido")
-    exit()
+    except ValueError as e:
+
+        print(e)
+
+while bonus_valido is not True:   
+    try:
+        bonus = input('Digite seu bonus ')
+        bonus_novo = float(bonus)
+
+        if bonus_novo <=0:
+            print('Não é aceito valor nulo ou menor que zero ')
+        else:
+            bonus_valido = True
+            print("Bonus valido")
+    except ValueError as e :
+        print(e)
 
 
-# 3) Solicita ao usuário que digite o valor do bônus recebido
+bonus_recebido = 1000 + salario_novo * bonus_novo  # Exemplo simples de KPI
 
-
-try:
-    bonus = float(input('Digite valor do bonus: '))
-    if bonus < 0:
-        print("O valor do bonus não pode ser negativo")
-        exit()
-except ValueError:
-    print("Digite um valor válido")
-    exit()
-
-
-# 4) Calcule o valor do bônus final 
-
-valor_bonus_final = (1000 + salario) * bonus
-
-# 5) Imprima cálculo do KPI para o usuário
-
-print(f'O valor do bonus final será de : {valor_bonus_final}')
-
-# 6) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
-print(f'{nome}, seu salario é de {salario} e valor de bonus final será de {valor_bonus_final}')
-
+# Imprime as informações para o usuário
+print(f"{nome}, seu salário é R${salario_novo:.2f} e seu bônus final é R${bonus_recebido:.2f}.")
